646cbde875b5ae427af3d5d2fbf5953255936d1b
deniz <oezdemir.de@gmail.com>
2024-06-28 18:17:19 +0200

    search added

--- Module_00/ex01/phonebook.cpp
+++ Module_00/ex01/phonebook.cpp
@@ -8,5 +8,55 @@ PhoneBook::Phonebook(){
 PhoneBook::~PhoneBook(){}
 
 void	PhoneBook::add_contact(){
-	//tbd
+	this->contacts[index].set_first_name(get_input("first name"));
+	this->contacts[index].set_last_name(get_input("last name"));
+	this->contacts[index].set_nickname(get_input("nickname"));
+	this->contacts[index].set_phone_number(get_input("phone number"));
+	this->contacts[index].set_darkest_secret(get_input("darkest secret"));
+	this->index++;
+	if (this->index == 8)
+	{
+		this->index == 0;
+		this->full = true;
+	}
+}
+
+void	PhoneBook::list_contacts(){
+	if (!this->full && this->index == 0)
+	{
+		std::cout << "Phonebook is empty.\n";
+		return;
+	}
+
+	std::cout << std::setw(10) << "     index" << '|'
+	<< std::setw(10) << "first name" << '|'
+	<< std::setw(10) << "last name" << '|'
+	<< std::setw(10) << "  nickname" << '|' << "\n"
+	<< std::setw(40) << std::setfill('-') << "\n";
+
+	int i = 0;
+	while (i < this->index || (this->full && i < 8))
+	{
+		std::cout << std::setw(10) << std::right << i + 1 << '|'
+		<< std::setw(10) << truncate(this->contacts[i].get_first_name()) << '|'
+		<< std::setw(10) << truncate(this->contacts[i].get_last_name()) << '|'
+		<< std::setw(10) << truncate(this->contacts[i].get_nickname()) << '|'
+		i++;
+	}
+	std:cout << std::setw(40) << std::setfill('-') << "\n";
+	this->select_contact();
+}
+
+
+
+std::string	get_input(std::string detail){
+	std::string	input;
+	std::cout << "\nPlease enter the " << detail << ": ";
+	while(input.empty())
+	{
+		std::getline(std::cin, input);
+		if (input.empty())
+			std::cout << "\nField " << detail << " can not be empty. Please try again: ";
+	}
+	return (input);
 }
