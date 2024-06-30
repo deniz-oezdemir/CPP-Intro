3bc41888e15d98c01d1dfda8d7fe944bb508e808
deniz <oezdemir.de@gmail.com>
2024-06-30 13:42:28 +0200

    works

--- Module_00/ex01/main.cpp
+++ Module_00/ex01/main.cpp
@@ -2,16 +2,22 @@
 
 int	main(){
 	PhoneBook book;
-	std::cout << "Crappy Awesome Phonebook\n";
-	std::cout << "Please enter ADD, SEARCH, or EXIT: ";
+	std::cout << "\n\n" << std::setw(40) << "Crappy Awesome Phonebook\n\n";
+	std::cout << "\nPlease enter ADD, SEARCH, or EXIT: ";
 
 	std::string cmd;
 	while (1){
 		std::getline(std::cin, cmd);
 		if (cmd == "ADD")
+		{
 			book.add_contact();
+			std::cout << "\nPlease enter ADD, SEARCH, or EXIT: ";
+		}
 		else if (cmd == "SEARCH")
+		{
 			book.list_contacts();
+			std::cout << "\nPlease enter ADD, SEARCH, or EXIT: ";
+		}
 		else if (cmd == "EXIT")
 			return 0;
 		else
--- Module_00/ex01/phonebook.cpp
+++ Module_00/ex01/phonebook.cpp
@@ -16,23 +16,26 @@ void	PhoneBook::add_contact(){
 	this->index++;
 	if (this->index == 8)
 	{
-		this->index == 0;
+		this->index = 0;
 		this->full = true;
 	}
+	std::cout << "\n";
 }
 
 void	PhoneBook::list_contacts(){
+	std::cout << "\n";
 	if (!this->full && this->index == 0)
 	{
 		std::cout << "Phonebook is empty.\n";
 		return;
 	}
 
-	std::cout << std::setw(10) << "     index" << '|'
+	std::cout
+	<< std::setw(10) << "     index" << '|'
 	<< std::setw(10) << "first name" << '|'
-	<< std::setw(10) << "last name" << '|'
-	<< std::setw(10) << "  nickname" << '|' << "\n"
-	<< std::setw(40) << std::setfill('-') << "\n";
+	<< std::setw(10) << " last name" << '|'
+	<< std::setw(10) << "  nickname" << "\n"
+	<< std::setw(44) << std::setfill('-') << "" << std::setfill(' ') << "\n";
 
 	int i = 0;
 	while (i < this->index || (this->full && i < 8))
@@ -43,13 +46,14 @@ void	PhoneBook::list_contacts(){
 		<< std::setw(10) << truncate_str(this->contacts[i].get_nickname()) << '\n';
 		i++;
 	}
-	std::cout << std::setw(40) << std::setfill('-') << "\n";
+	std::cout << std::setw(44) << std::setfill('-') << "" << std::setfill(' ') << "\n";
 	this->select_contact();
+	std::cout << "\nPlease enter ADD, SEARCH, or EXIT: ";
 }
 
 void	PhoneBook::select_contact(){
 	std::string	input;
-	std::cout << "Enter the index of the contact to show: ";
+	std::cout << "\nEnter the index of the contact to show: ";
 	while (1)
 	{
 		std::getline(std::cin, input);
@@ -57,18 +61,18 @@ void	PhoneBook::select_contact(){
 			((this->full && input[0] > 48 && input[0] < 57) ||
 			(!this->full && input[0] > 48 && input[0] <= 48 + this->index))){
 				std::cout << "\n"
-				<< std::setw(10) << std::left << "first name"
-				<< std::setw(0) << this->contacts[input[0] - 49].get_first_name()
-				<< std::setw(10) << std::left << "last name"
-				<< std::setw(0) << this->contacts[input[0] - 49].get_last_name()
-				<< std::setw(10) << std::left << "nickname"
-				<< std::setw(0) << this->contacts[input[0] - 49].get_nickname()
-				<< std::setw(10) << std::left << "phone number"
-				<< std::setw(0) << this->contacts[input[0] - 49].get_phone_number()
+				<< std::setw(10) << std::left << "first name: "
+				<< std::setw(0) << this->contacts[input[0] - 49].get_first_name() << "\n"
+				<< std::setw(10) << std::left << "last name: "
+				<< std::setw(0) << this->contacts[input[0] - 49].get_last_name() << "\n"
+				<< std::setw(10) << std::left << "nickname: "
+				<< std::setw(0) << this->contacts[input[0] - 49].get_nickname() << "\n"
+				<< std::setw(10) << std::left << "phone number: "
+				<< std::setw(0) << this->contacts[input[0] - 49].get_phone_number() << "\n"
 				<< "\n";
 				return;
 		}
-		std::cout << "Choice invalid. Please choose an existing index: ";
+		std::cout << "\nChoice invalid. Please choose an existing index: ";
 	}
 }
 
