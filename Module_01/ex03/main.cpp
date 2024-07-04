4761afae23d4671d5417de4c143d0412749d79dc
deniz <oezdemir.de@gmail.com>
2024-07-04 11:27:11 +0200

    m01e03

---
+++ Module_01/ex03/main.cpp
@@ -0,0 +1,24 @@
+#include "HumanA.hpp"
+#include "HumanB.hpp"
+
+/* HumanA: reference to Weapon
+HumanB: pointer to Weapon as initialized without Weapon and reference can not be NULL */
+
+int main(){
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+	}
+	{
+		Weapon club = Weapon("crude spiked club");
+		HumanB jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+	return 0;
+}
